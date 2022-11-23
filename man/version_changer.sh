694b1f6d804730fc4c8771bc11f06de3e1210fcd
k_lar <zan.sprogar@gmail.com>
2022-11-23 20:55:28 +0100

    Version changer script for my convenience

---
+++ man/version_changer.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+date=$(date '+%B %d, %Y')
+version=$(sh -c ".././brem --version")
+
+sed -i "s/date\:.*/date: $date/g" brem.1.md
+
+printf '%s is the current brem version.\n' "$version"
+printf "What version do you want to change it to?\n: v"; read -r ver_choice
+
+sed -i "s/version=.*/version=\"v$ver_choice\"/" ../brem
+printf 'Successfully updated version number to %s in brem.\n' "$ver_choice"
+
+sed -i "s/footer\:.*/footer: brem $ver_choice/" brem.1.md
+printf 'Successfully updated version number to %s in brem.1.md\n' "$ver_choice"
+exit 0
+
