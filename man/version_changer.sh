e7112a7303c8ea762b2367a4c9cb5c5b7045b871
k_lar <zan.sprogar@gmail.com>
2022-11-24 15:06:01 +0100

    Added extra step to generate manpage

--- man/version_changer.sh
+++ man/version_changer.sh
@@ -14,5 +14,12 @@ printf 'Successfully updated version number to %s in brem.\n' "$ver_choice"
 
 sed -i "s/footer\:.*/footer: brem $ver_choice/" brem.1.md
 printf 'Successfully updated version number to %s in brem.1.md\n' "$ver_choice"
+
+printf "Do you want to generate new manpage?\n[Y/n]: "; read -r man_choice
+if ! [ "$man_choice" = "n" ]; then
+    cd ..
+    make manpage
+fi
+
 exit 0
 
